0ee43933aa0acafd07f9a804497a8800d57205ae
Ahanaful <ahanaful11@gmail.com>
2025-04-22 21:46:31 -0500

    Diet button working now

--- app/src/main/java/com/example/phms/HomePage.kt
+++ app/src/main/java/com/example/phms/HomePage.kt
@@ -17,6 +17,7 @@ class HomePage : AppCompatActivity() {
     lateinit var  vitalButton: TextView
     lateinit var  medButton: TextView
     lateinit var notesButton: TextView
+    lateinit var  dietButton: TextView
 
     lateinit var auth: FirebaseAuth
     var db = FirebaseFirestore.getInstance()
@@ -34,6 +35,7 @@ class HomePage : AppCompatActivity() {
         nameText = findViewById(R.id.nameText)
         vitalButton = findViewById(R.id.vital_signs)
         medButton = findViewById(R.id.med)
+        dietButton = findViewById(R.id.placeholder3)
 
         auth = FirebaseAuth.getInstance()
 
@@ -62,6 +64,10 @@ class HomePage : AppCompatActivity() {
         }
         medButton.setOnClickListener {
             val intent = Intent(this, Medications::class.java)
+            startActivity(intent)
+        }
+        dietButton.setOnClickListener {
+            val intent = Intent(this, Diet::class.java)
             startActivity(intent)
         }
 
